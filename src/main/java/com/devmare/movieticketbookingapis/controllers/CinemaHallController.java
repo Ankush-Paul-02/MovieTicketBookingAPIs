be03813171a8a847eeddfdc521545b5e2e0ece52
Ankush-Paul-02 <ankushpaulclg2002@gmail.com>
2023-10-20 01:10:27 +0530

    implement cinemahall controller APIs

--- src/main/java/com/devmare/movieticketbookingapis/controllers/CinemaHallController.java
+++ src/main/java/com/devmare/movieticketbookingapis/controllers/CinemaHallController.java
@@ -1,11 +1,14 @@
 package com.devmare.movieticketbookingapis.controllers;
 
+import com.devmare.movieticketbookingapis.payloads.AppApiResponse;
 import com.devmare.movieticketbookingapis.payloads.DTO.CinemaHallDTO;
 import com.devmare.movieticketbookingapis.services.CinemaHallService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -24,4 +27,35 @@ public class CinemaHallController {
         return new ResponseEntity<>(newCinemaHallDTO, HttpStatus.CREATED);
     }
 
+    //! http://localhost:8081/api/v1/cinemaHall/{cinemaHallId}
+    @GetMapping("/cinemaHall/{cinemaHallId}")
+    public ResponseEntity<CinemaHallDTO> getCinemaHallById(
+            @PathVariable Integer cinemaHallId
+    ) {
+        return ResponseEntity.ok(cinemaHallService.getCinemaHallById(cinemaHallId));
+    }
+
+    //! http://localhost:8081/api/v1/cinemaHall/
+    @GetMapping("/cinemaHall/")
+    public ResponseEntity<List<CinemaHallDTO>> getAllCinemaHall() {
+        return ResponseEntity.ok(cinemaHallService.getAllCinemasHalls());
+    }
+
+    //! http://localhost:8081/api/v1/cinemaHall/{cinemaHallId}
+    @PutMapping("/cinemaHall/{cinemaHallId}")
+    public ResponseEntity<CinemaHallDTO> updateCinemaHall(
+            @RequestBody CinemaHallDTO cinemaHallDTO,
+            @PathVariable Integer cinemaHallId
+    ) {
+        return ResponseEntity.ok(cinemaHallService.updateCinemaHall(cinemaHallDTO, cinemaHallId));
+    }
+
+    //! http://localhost:8081/api/v1/cinemaHall/{cinemaHallId}
+    @DeleteMapping("/cinemaHall/{cinemaHallId}")
+    public ResponseEntity<AppApiResponse> deleteCinemaHall(
+            @PathVariable Integer cinemaHallId
+    ) {
+        cinemaHallService.deleteCinemaHall(cinemaHallId);
+        return new ResponseEntity<>(new AppApiResponse("Cinema hall deleted successfully!", true), HttpStatus.OK);
+    }
 }
